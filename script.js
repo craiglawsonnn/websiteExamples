26dc370956d243ed4807032cfcc654b36d6524f5
Craig Lawson <78158337+craiglawsonnn@users.noreply.github.com>
2024-10-23 19:03:43 +0100

    Implementing Formspree

--- script.js
+++ script.js
@@ -21,8 +21,23 @@ document.addEventListener('DOMContentLoaded', (event) => {
     if (contactForm) {
         contactForm.addEventListener('submit', function(e) {
             e.preventDefault();
-            alert('Thank you for your message! We will get back to you soon.');
-            contactForm.reset();
+            const formData = new FormData(this);
+            fetch(this.action, {
+                method: 'POST',
+                body: formData,
+                headers: {
+                    'Accept': 'application/json'
+                }
+            }).then(response => {
+                if (response.ok) {
+                    alert('Thank you for your message! We will get back to you soon.');
+                    contactForm.reset();
+                } else {
+                    alert('Oops! There was a problem submitting your form');
+                }
+            }).catch(error => {
+                alert('Oops! There was a problem submitting your form');
+            });
         });
     }
 });
